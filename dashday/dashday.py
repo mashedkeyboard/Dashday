743e0c1f4c5e3be754e38042841a50611de0b9e8
mashedkeyboard <curtis@mashedkeyboard.me>
2016-08-02 15:00:40 +0100

    Added more debug logging

--- dashday/dashday.py
+++ dashday/dashday.py
@@ -86,23 +86,30 @@ def main():
 
     # Setup the printer for the beautiful header, and then print it
     p.set("LEFT", "B", "B", 2, 2)
+    logging.debug("Set the header printing style")
     p.text("Hello,\n" + maincfg['HelloMyNameIs'] + '\n\n')
+    logging.debug("Printed the header")
     
     # Time to reset the font
     p.set("LEFT", "A", "normal", 1, 1)
+    logging.debug("Unset the header printing style")
     
     # Now let's fetch the more localized and up-to-date but raw data - with an accurate 'real' OS path?
     p.image(os.path.join(os.path.realpath('resources/images/weather') + os.path.sep + wtypes[datapoint.fetchFrcWthrType(datapointcfg['ForecastLocation'],datapointcfg['DataPointKey'])]))
+    logging.debug("Printed the localized weather data image")
 
     # And now we can find the text forecast from the Met Office's DataPoint API
     p.text(datapoint.fetchRegionFrcAsText(datapointcfg['TextRegionCode'],datapointcfg['DataPointKey']))
+    logging.debug("Printed the regional weather text")
     
     # Cut the paper! Magic!
     p.cut()
+    logging.debug("Printed the localized weather data image")
 
     # If we're in test mode, print the results to the screen
     if debugcfg['TestMode']:
         print(str(p.output))
+        logging.debug("Printed dummy output to stdout")
        
     # Call the "wahey I'm done!" handler
     handlers.closed()
