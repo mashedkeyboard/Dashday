9fff7f25569e11bb72954bea7e46884963824d72
Curtis // mashedkeyboard <curtis@mashedkeyboard.me>
2016-08-16 14:58:24 +0100

    Added settings page to the web interface

--- dashday/dashday.py
+++ dashday/dashday.py
@@ -8,40 +8,60 @@ import os
 import threading
 
 global dashdayjob
+global configfile
 
 def start():
     global dashdayjob
     print("Dashday started at " + time.strftime("%d/%m/%y %H:%M:%S"))    
     main.start()
-    serv.updateScheduledRun(dashdayjob.next_run.strftime("%dd/%mm/%yyyy %H:%M:%S"))
-    print("Dashday completed, next scheduled launch is at " + dashdayjob.next_run.strftime("%dd/%mm/%yyyy %H:%M:%S"))
+    serv.updateScheduledRun(dashdayjob.next_run.strftime("%d/%m/%y %H:%M:%S"))
+    print("Dashday completed, next scheduled launch is at " + dashdayjob.next_run.strftime("%d/%m/%y %H:%M:%S"))
 
 def runapp():
     global dashdayjob
+    global configfile
     dashdayjob = schedule.every().day.at(configfile['Schedule']['runat']).do(start)
     serv.updateScheduledRun(dashdayjob.next_run.strftime("%d/%m/%y %H:%M:%S"))
-    print("Dashday service running, next scheduled launch is at " + dashdayjob.next_run.strftime("%dd/%mm/%yyyy %H:%M:%S"))
+    print("Dashday service running, next scheduled launch is at " + dashdayjob.next_run.strftime("%d/%m/%y %H:%M:%S"))
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-# Load the configuration file
-if os.path.isfile('config/dashday.cfg') == True:
-    configfile = configparser.ConfigParser()
-    try:
-        configfile.read('config/dashday.cfg')
-    except PermissionError:
-        handlers.criterr("Permissions error on dashday.cfg. Please ensure you have write permissions for the directory.")
-else:
-    print("No configuration file found. Please configure Dashday.")
-    exit()
+def reload():
+    global dashdayjob
+    global configfile
+    schedule.clear()
+    if os.path.isfile('config/dashday.cfg') == True:
+        configfile = configparser.ConfigParser()
+        try:
+            configfile.read('config/dashday.cfg')
+        except PermissionError:
+            handlers.criterr("Permissions error on dashday.cfg. Please ensure you have write permissions for the directory.")
+    else:
+        print("No configuration file found. Please configure Dashday.")
+        exit()
+    dashdayjob = schedule.every().day.at(configfile['Schedule']['runat']).do(start)
+    serv.updateScheduledRun(dashdayjob.next_run.strftime("%d/%m/%y %H:%M:%S"))
 
-# Initialize and then run the web server
-serv.init()
 
-thr = threading.Thread(target=serv.run)
-thr.start()
+if __name__ == "__main__":
+    # Load the configuration file
+    if os.path.isfile('config/dashday.cfg') == True:
+        configfile = configparser.ConfigParser()
+        try:
+            configfile.read('config/dashday.cfg')
+        except Exception as e:
+            handlers.criterr("Permissions error on dashday.cfg. Please ensure you have write permissions for the directory.")
+    else:
+        print("No configuration file found. Please configure Dashday.")
+        exit()
 
-# Runs the scheduler and all that jazz
-thr = threading.Thread(target=runapp)
-thr.start()
+    # Initialize and then run the web server
+    serv.init()
+
+    thr = threading.Thread(target=serv.run)
+    thr.start()
+
+    # Runs the scheduler and all that jazz
+    thr = threading.Thread(target=runapp)
+    thr.start()
--- dashday/web/serv.py
+++ dashday/web/serv.py
@@ -1,18 +1,59 @@
-from tg import expose, TGController, AppConfig
+from tg import expose, TGController, AppConfig, redirect
 import configparser
 import webhelpers2
 import webhelpers2.text
 from web.mtwsgi import make_server
 import os
+import dashday
 
 global nextRunTime
 global userconfig
 
 class RootController(TGController):
     @expose('web/views/index.xhtml')
-    def index(self):
+    def index(self,update = ''):
         global nextRunTime
-        return {'nextRun': nextRunTime}
+        return {'nextRun': nextRunTime, 'update': update}
+
+    @expose('web/views/settings.xhtml')
+    def settings(self):
+        mainconfig = configparser.ConfigParser()
+        try:
+            mainconfig.read('config/dashday.cfg')
+        except PermissionError:
+            handlers.criterr("Permissions error on dashday.cfg. Please ensure you have write permissions for the directory.")
+        return {'runTime': mainconfig["Schedule"]["runat"],
+                'name': mainconfig["General"]["HelloMyNameIs"],
+                'pvend': mainconfig["General"]["Vendor"],
+                'pprod': mainconfig["General"]["Product"]}
+
+    @expose()
+    def changeTime(self,scheduleRun):
+        mainconfig = configparser.ConfigParser()
+        try:
+            mainconfig.read('config/dashday.cfg')
+        except PermissionError:
+            handlers.criterr("Permissions error on dashday.cfg. Please ensure you have write permissions for the directory.")
+        mainconfig["Schedule"]["runat"] = scheduleRun
+        with open('config/dashday.cfg', 'w') as dashdaycfg:
+            mainconfig.write(dashdaycfg)
+        dashday.reload()
+        redirect('/', {'update': 'true'})
+
+    @expose()
+    def changeGeneral(self,name,pvend,pprod):
+        mainconfig = configparser.ConfigParser()
+        try:
+            mainconfig.read('config/dashday.cfg')
+        except PermissionError:
+            handlers.criterr("Permissions error on dashday.cfg. Please ensure you have write permissions for the directory.")
+        mainconfig["General"]["HelloMyNameIs"] = name
+        mainconfig["General"]["vendor"] = pvend
+        mainconfig["General"]["product"] = pprod
+        with open('config/dashday.cfg', 'w') as dashdaycfg:
+            mainconfig.write(dashdaycfg)
+        dashday.reload()
+        redirect('/', {'update': 'true'})
 
 def init():
     global userconfig
