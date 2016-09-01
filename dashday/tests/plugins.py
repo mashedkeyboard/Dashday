f5874283603bcf4a166db7aa689a9b5b8a8ceaff
Curtis // mashedkeyboard <curtis@mashedkeyboard.me>
2016-09-01 14:55:30 +0100

    Fixed test string

--- dashday/tests/plugins.py
+++ dashday/tests/plugins.py
@@ -13,5 +13,5 @@ def testLoadPlugin():
 
     p = printer.Dummy()
     pluginloader.printPlugin('test',p)
-    if p.output.decode("utf-8")  != "\x1bD\x04\rI am a test. This is a test. Testing stuff. Test. Ta!\x1bD\x04\r":
+    if p.output.decode("utf-8")  != "I am a test. This is a test. Testing stuff. Test. Ta!":
         raise escpos.Error("Can't print test plugin successfully.")
